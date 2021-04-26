0ad0651be0169e2e604e0210c09f7fc0704cf602
jsmaster <mike@jsmaster.ru>
2021-04-26 18:09:30 +0400

    Создание проекта

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">{{title}}</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      title: 'Title Header'
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+.header {
+  height: 150px;
+  background: #eee;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/scss/main.scss'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
