2e3171bc8dc40e9dffa15845e3f3fdcaf9273def
jsmaster <mike@jsmaster.ru>
2021-05-05 08:59:50 +0400

    Проработка футера

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Footer</h1>
+  <div class="footer">
+    <b-container>
+      <b-row>
+        <b-col sm="2">
+          <div class="footer__logo logo">Logo</div>
+        </b-col>
+        <b-col sm="5">
+          <nav class="footer__menu menu">
+            <router-link to="/">Главная</router-link>
+            |
+            <router-link to="/about">О нас</router-link>
+            |
+            <router-link to="/dashboard">Личный кабинет</router-link>
+          </nav>
+        </b-col>
+        <b-col sm="3">
+          <p>Copyright &copy; All right reserved</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
+
+<style>
+.footer__menu {
+}
+</style>
